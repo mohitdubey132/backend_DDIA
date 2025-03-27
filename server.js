dc492d07a534714fc18f4e70b1a4acd4a32ad0d5
mohit.dubey@webztechie.com-at-092438096643 <mohit.dubey@webztechie.com>
2025-03-27 20:00:18 +0530

    remove os module work

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 import 'dotenv/config.js'
 import express from 'express';
-import os from 'node:os'
  const app = express()
  const port = process.env.PORT||3000;
  // * Middleware
@@ -20,8 +19,7 @@ import routes from "./Routes/index.js";
 // console.log(solt)
 app.use(routes);
  app.listen(port,()=>{
-   console.log("Total Memory (RAM):", os.totalmem(), "bytes");
-console.log("Total Memory (GB):", (os.totalmem() / (1024 ** 3)).toFixed(2), "GB");
+
     console.log("app is working on port",port)
  })
 
